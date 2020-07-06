67457505482b3b2c1d64b41635c0ecbee0d559d7
elyyps <ahmed.ely.mabrouk@gmail.com>
2020-07-06 08:46:52 +0100

    fix: changed phone number in contact info

--- api/modules/contact/dummy-data.ts
+++ api/modules/contact/dummy-data.ts
@@ -5,7 +5,7 @@ export const contactEnglishData = (): IContact => ({
   description: "",
   subTitle: "Hiway office",
   address: "Kerkenbos 1075-V \n 6546 BB Nijmegen",
-  phoneNumber: "+31(0) 88 711 3333",
+  phoneNumber: "+31(0) 88 711 3300",
   email: "info@hiway.io",
   mapImage: "/img/optimized/map.png",
 });
@@ -14,7 +14,7 @@ export const contactDutchData = (): IContact => ({
   description: "",
   subTitle: "Hiway office",
   address: "Kerkenbos 1075-V \n 6546 BB Nijmegen",
-  phoneNumber: "+31(0) 88 711 3333",
+  phoneNumber: "+31(0) 88 711 3300",
   email: "info@hiway.io",
   mapImage: "/img/optimized/map.png",
 });
