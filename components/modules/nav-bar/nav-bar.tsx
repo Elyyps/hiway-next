f259849f1a29baf6628d63914840d781150e6c4e
elyyps <ahmed.ely.mabrouk@gmail.com>
2020-07-08 15:39:38 +0100

    fix: fixed nav bar

--- components/modules/nav-bar/nav-bar.tsx
+++ components/modules/nav-bar/nav-bar.tsx
@@ -9,6 +9,7 @@ import { LanguageContext } from "../../../context/language";
 import DropdownLinkComponent from "../../cores/dropdown-link/dropdown-link";
 import RouteContext from "../../../context/route";
 import { useRouter } from "next/router";
+import ScreenWidthContext from "../../../context/screen-width";
 
 interface INavBarComponentProps {
   navBarModule: INavBar;
@@ -17,6 +18,8 @@ const NavBarComponent = (props: INavBarComponentProps) => {
   const [isClosed, setIsClosed] = React.useState(false);
   const { selectLanguage, language } = React.useContext(LanguageContext);
   const { route } = React.useContext(RouteContext);
+  const { windowSize } = React.useContext(ScreenWidthContext);
+
   const router = useRouter();
   const onLanguageSelected = (text: string) => {
     setIsClosed(!isClosed);
@@ -29,81 +32,99 @@ const NavBarComponent = (props: INavBarComponentProps) => {
   };
 
   return (
-    <div className={` ${style["nav-bar"]} container`}>
-      <div className={style["nav-bar-logo"]} onClick={handleLogoClick}>
-        <img src={props.navBarModule.logo} alt="hiway logo" />
-      </div>
-      <div className={` ${style["nav-bar-menu"]} ${"uk-visible@s"} `}>
-        <ul>
-          {props.navBarModule.items.map((item, key) => (
-            <li
-              key={key}
-              className={style[route === item.title.text ? "selected" : ""]}
-            >
-              {item.dropdownLink ? (
-                <DropdownComponent
-                  title={item.title.text}
-                  fullWidth
-                  // isClosed={route !== item.title.text && true}
-                >
-                  <div className={style["nav-bar-dropdown"]}>
-                    <div className="uk-container uk-container-small">
-                      <div className={style["dropdown-left"]}>
-                        <span>{item.dropdownTitle && item.dropdownTitle}</span>
-                        <div>{item.dropdownText}</div>
-                      </div>
-                      <div className={style["dropdown-right"]}>
-                        {item.dropdownLink.map((link, key) => (
-                          <div key={key}>
-                            <DropdownLinkComponent {...link} />
-                          </div>
-                        ))}
+    <div className={style["nav-bar"]}>
+      <div className={` ${style["nav-bar-container"]} container`}>
+        <div
+          className={style["nav-bar-logo"]}
+          onClick={handleLogoClick}
+          style={{ height: windowSize > 639 ? windowSize / 28 : "" }}
+        >
+          <img src={props.navBarModule.logo} alt="hiway logo" />
+        </div>
+        <div className={` ${style["nav-bar-menu"]} ${"uk-visible@s"} `}>
+          <ul>
+            {props.navBarModule.items.map((item, key) => (
+              <li
+                key={key}
+                className={style[route === item.title.text ? "selected" : ""]}
+              >
+                {item.dropdownLink ? (
+                  <DropdownComponent title={item.title.text} fullWidth>
+                    <div className={style["nav-bar-dropdown"]}>
+                      <div className="uk-container uk-container-small">
+                        <div className={style["dropdown-left"]}>
+                          <span>
+                            {item.dropdownTitle && item.dropdownTitle}
+                          </span>
+                          <div>{item.dropdownText}</div>
+                        </div>
+                        <div className={style["dropdown-right"]}>
+                          {item.dropdownLink.map((link, key) => (
+                            <div key={key}>
+                              <DropdownLinkComponent {...link} />
+                            </div>
+                          ))}
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </DropdownComponent>
-              ) : (
-                <LinkComponent to={item.title.url ? item.title.url : ""}>
-                  {item.title.text}
-                </LinkComponent>
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className={style["nav-bar-language"]}>
-        <div className={`  ${"uk-hidden@s"} `}>
-          <DropdownComponent title={language} variant="primary">
-            <ul>
-              <li onClick={() => onLanguageSelected("EN")}>English</li>
-              <li onClick={() => onLanguageSelected("NL")}>Dutch</li>
-            </ul>
-          </DropdownComponent>
+                  </DropdownComponent>
+                ) : (
+                  <LinkComponent to={item.title.url ? item.title.url : ""}>
+                    {item.title.text}
+                  </LinkComponent>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={`${style["nav-bar-language"]}`}>
+          <div className={"uk-hidden@s"}>
+            <DropdownComponent title={language} variant="primary">
+              <ul className={style["language-list"]}>
+                <li onClick={() => onLanguageSelected("EN")}>English</li>
+                <li onClick={() => onLanguageSelected("NL")}>Dutch</li>
+              </ul>
+            </DropdownComponent>
+          </div>
+          <div
+            className={`${style["nav-bar-language-desktop"]} ${style["large"]} uk-visible@s `}
+          >
+            <span>Language :</span>
+            <DropdownComponent
+              title={language === "EN" ? "English" : "Dutch"}
+              variant="primary"
+            >
+              <ul className={style["language-list"]}>
+                <li onClick={() => onLanguageSelected("EN")}>English</li>
+                <li onClick={() => onLanguageSelected("NL")}>Dutch</li>
+              </ul>
+            </DropdownComponent>
+          </div>
+        </div>
+
+        <div className={` uk-hidden@s ${style["nav-bar-search"]} `}>
+          <span>
+            <NavBarModalComponent icon={props.navBarModule.menuIcon}>
+              <HamburgerMenuComponent menuItems={props.navBarModule.items} />
+            </NavBarModalComponent>
+          </span>
         </div>
       </div>
-
-      <div className={` uk-hidden@s ${style["nav-bar-search"]} `}>
-        <span>
-          <NavBarModalComponent icon={props.navBarModule.menuIcon}>
-            <HamburgerMenuComponent menuItems={props.navBarModule.items} />
-          </NavBarModalComponent>
-        </span>
+      <div
+        className={`${style["nav-bar-language-desktop"]} ${style["x-large"]}`}
+      >
+        <span>Language :</span>
+        <DropdownComponent
+          title={language === "EN" ? "English" : "Dutch"}
+          variant="primary"
+        >
+          <ul className={style["language-list"]}>
+            <li onClick={() => onLanguageSelected("EN")}>English</li>
+            <li onClick={() => onLanguageSelected("NL")}>Dutch</li>
+          </ul>
+        </DropdownComponent>
       </div>
     </div>
   );
 };
 export default NavBarComponent;
-{
-  /* <div className="uk-visible@s">
-<span>Language :</span>
-<DropdownComponent
-  title={language === "EN" ? "English" : "Dutch"}
-  variant="primary"
->
-  <ul>
-    <li onClick={() => onLanguageSelected("EN")}>English</li>
-    <li onClick={() => onLanguageSelected("NL")}>Dutch</li>
-  </ul>
-</DropdownComponent>
-</div> */
-}
