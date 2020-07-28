a394a3f582b41da2a8357f47d9864fcfc5937b34
elyyps <ahmed.ely.mabrouk@gmail.com>
2020-07-28 09:13:30 +0100

    fix: fixing issues

--- api/modules/newsletter/dummy-data.ts
+++ api/modules/newsletter/dummy-data.ts
@@ -4,7 +4,7 @@ export const newsletterEnglishData = (): INewsletter => ({
   title: "Want more information about Hiway?",
   info: "or check out our",
   link: { text: "about page", url: "/about" },
-  button: { text: "Get in touch" },
+  button: { text: "Request a demo" },
   card: {
     title: "Newsletter",
     description:
@@ -16,7 +16,7 @@ export const newsletterDutchData = (): INewsletter => ({
   title: "Zou je meer willen weten over Hiway?",
   link: { text: "about pagina", url: "/about" },
   info: "Of bezoek onze",
-  button: { text: "Neem contact op" },
+  button: { text: "Demo aanvragen" },
   card: {
     title: "Newsletter",
     description:
