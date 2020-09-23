d35a5631ef6c772efd3780a2239c78e3ff84ca75
elyyps <ahmed.ely.mabrouk@gmail.com>
2020-09-23 19:55:08 +0100

    fix: removed sendgrid key

--- utils/send-email.ts
+++ utils/send-email.ts
@@ -1,14 +1,12 @@
 import fetch from "node-fetch";
 
 const SENDGRID_API = "https://api.sendgrid.com/v3/mail/send";
-const SENDGRID_API_KEY =
-  "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";
 const sendEmail = async (name: string, email: string) => {
   await fetch(SENDGRID_API, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${SENDGRID_API_KEY}`,
+      //  Authorization: `Bearer ${SENDGRID_API_KEY}`,
     },
     body: JSON.stringify({
       personalizations: [
